3476c5e29f9cb421748a3166c8c0b3cc7df8739f
manish-jandu <manishjandu1@gmail.com>
2023-06-09 21:20:43 +0530

    sample express server + nodemon setup

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-console.log("Hello world");
+const express = require("express");
+const app = express();
+
+const port = 4001;
+
+app.get("/",(req,res) => { 
+    res.status(200).json({message:"Pinging Success"});
+});
+
+app.listen(port,()=>{
+    console.log("Server started at port ",port);
+});
