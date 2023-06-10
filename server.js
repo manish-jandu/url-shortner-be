1c98dce311e6ea7be8f6b0b21de8b0c89b5e7d73
manish-jandu <manishjandu1@gmail.com>
2023-06-10 12:30:27 +0530

    mongoose setup in project

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const errorHanlder = require("./middleware/errorHandler");
+const connectDb = require("./config/dbConnection");
 const app = express();
 require("dotenv").config();
 
 const port = process.env.PORT;
+
+connectDb();
 
 app.use(express.json());
 app.use('/api/urls',require("./router/urlRoutes"));
