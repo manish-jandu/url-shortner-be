0973aadb6c07009b2094c19d340cfad4b244aa59
manish-jandu <manishjandu1@gmail.com>
2023-06-18 23:44:54 +0530

    sheduler setup to delete url

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const errorHanlder = require("./middleware/errorHandler");
 const connectDb = require("./config/dbConnection");
+const {startSheduler} = require("./scripts/deleteUrl");
 const app = express();
 require("dotenv").config();
 
@@ -12,6 +13,8 @@ app.use(express.json());
 app.use('/api/urls',require("./router/urlRoutes"));
 app.use('/',require("./router/redirectRoute"));
 app.use(errorHanlder);
+
+startSheduler()
 
 app.listen(port,()=>{
     console.log("Server started at port ",port);
