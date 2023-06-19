7fa9e6eac4f69e9b255d6cda90809066b805243e
manish-jandu <manishjandu1@gmail.com>
2023-06-19 20:38:32 +0530

    backend and frontend directory setup

--- router/redirectRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const {redirect} = require("../controller/redirectController");
-
-router.route("/:id").get(redirect);
-
-module.exports = router
