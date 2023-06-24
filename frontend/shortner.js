812811466b55f416e5b6d04e61be90ff4402c1ea
manish-jandu <manishjandu1@gmail.com>
2023-06-24 23:32:34 +0530

    post request setup for shortner

--- frontend/shortner.js
+++ frontend/shortner.js
@@ -1,11 +1,33 @@
 
-function checkUrl(text){
+async function checkAndGetShortUrl(text){
+    if(!text){
+        return null;
+    }
+
+    const response = await fetch("http://localhost:4001/api/urls",{
+        method: 'POST',
+        headers:{
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+        },
+        body: JSON.stringify({
+            url:text
+        })
+    });
+    const body = await response.json();
+    document.getElementById("short-url-text").innerText = body.short_url
+    document.getElementById("short-url-text").href = body.short_url
+    document.getElementById("orignal-url-text").innerText = body.orig_url
     
 }
 
 function onShortUrlBtnClick(){
     console.log("short click")
-    const text = document.getElementById("url-short-input-text")
+    const text = document.getElementById("url-short-input-text").value;
+    const newUrl = checkAndGetShortUrl(text);
+    if(!newUrl){
+        //url was empty or BE thrown an error.
+    }
 }
 
 function onCopyBtnClick(){
